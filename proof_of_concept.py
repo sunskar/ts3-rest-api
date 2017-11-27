082b91ff0ecf7c6d65326eb65b3e257c891b9bd7
Ludvig <ludvigch@kth.se>
2017-11-27 21:55:29 +0100

    kek fixin fuked shiet

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,67 +1,21 @@
-import telnetlib
+import ts3_rest_api as tra
+
 
 def main():
 
     HOST = 'host'
-    PORT = 'port'
-    USER = 'username'
-    PASS = 'password'
+    PORT = '10011'
+    USER = 'user'
+    PASS = 'pass'
     
-    tn = connectToTelnet(HOST, PORT, USER, PASS)
-    list_of_channels = getChannelList(tn)
-    print getChannelByCid(8, list_of_channels)['channel_name']
-    for client in getClientList(tn):
+    tn = tra.connectToTelnet(HOST, PORT, USER, PASS)
+    list_of_channels = tra.getChannelList(tn)
+    print tra.getChannelByCid(8, list_of_channels)['channel_name']
+    for client in tra.getClientList(tn):
         if client['client_type'] != '0':
             continue
-        print (str(client['client_nickname']) + ' - ' + str(getChannelByCid(client['cid'], list_of_channels)['channel_name']))
-
-
-def connectToTelnet(host, port, user, password):
-    telnet = telnetlib.Telnet(host, port)
-    telnet.read_until('\n\r')
-    telnet.read_until('\n\r')
-    telnet.write('login ' + user + ' ' + password + '\r\n')
-    telnet.read_until('\n\r')
-    telnet.write('use 1\r\n')
-    telnet.read_until('\n\r')
-    return telnet
-
-def getChannelList(telnet):
-
-    telnet.write('channellist\r\n')
-    channellist = telnet.read_until('\n\r')[:-2]
-    telnet.read_until('\n\r')
-    channels = channellist.split('|')
-    list_of_channels = []
-    for channel in channels:
-        channelinfo = {}
-        for token in channel.split(' '):
-            info = token.split('=')
-            channelinfo[info[0]] = info[1]
-        list_of_channels.append(channelinfo)
-    return list_of_channels
-
-
-def getChannelByCid(cid, list_of_channels):
-    for channel in list_of_channels:
-        if int(channel['cid']) == int(cid):
-            return channel
-    return -1
-
-def getClientList(telnet):
-    telnet.write('clientlist\r\n')
-    clientlist = telnet.read_until('\n\r')[:-2]
-    telnet.read_until('\n\r')
-    clients = clientlist.split('|')
-    list_of_clients = []
-    for client in clients:
-        clientinfo = {}
-        for token in client.split(' '):
-            info = token.split('=')
-            clientinfo[info[0]] = info[1]
-        list_of_clients.append(clientinfo)
-    return list_of_clients
-
+        print (str(client['client_nickname']) + ' - ' + str(tra.getChannelByCid(client['cid'], list_of_channels)['channel_name']))
 
 if __name__ == "__main__":
     main()
+
