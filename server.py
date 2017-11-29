798cd81bedb826becb365622c7c98ccaa0a8d0b3
Ludvig <ludvigch@kth.se>
2017-11-29 20:40:14 +0100

    added flask PoC

--- server.py
+++ server.py
@@ -1,13 +1,32 @@
 # flask server for handling the requests
-from flask import Flask
-import ts3_rest_api
+
+from flask import Flask, jsonify
+import ts3_rest_api as api
+
+HOST = '127.0.0.1'
+PORT = '10011'
+USER = 'serveradmin'
+PASS = 'v8yL9+VH'
+
+tn = api.connectToTelnet(HOST, PORT, USER, PASS)
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/api/')
 def index():
     return "hello"
 
 
+@app.route('/api/channels/', methods=['GET'])
+def get_channels():
+    return jsonify( api.getChannelList(tn) )
 
+@app.route('/api/channels/<int:cid>')
+def get_channel(cid):
+    return jsonify( api.getChannelByCid(cid, api.getChannelList(tn)) )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
